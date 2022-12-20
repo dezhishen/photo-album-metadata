65f53b5142efbf4ab4202e9c9cb77999d4a92dbb
dezhiShen <1179551960@qq.com>
2022-12-20 19:52:09 +0800

    feat:补充Artist字段

--- pkg/model/artist.go
+++ pkg/model/artist.go
@@ -1,4 +1,30 @@
 package model
 
+// artist nfo file
+// <?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
 type Artist struct {
+	Name                string   `xml:"name"`
+	MusicBrainzArtistID string   `xml:"musicBrainzArtistID"`
+	SortName            string   `xml:"sortname"`
+	Type                string   `xml:"type"`
+	Disambiguation      string   `xml:"disambiguation"`
+	Genre               string   `xml:"genre"`
+	Style               []string `xml:"style"`
+	Mood                []string `xml:"mood"`
+	YearsActive         string   `xml:"yearsactive"`
+	Born                string   `xml:"born"`
+	Formed              string   `xml:"formed"`
+	Biography           string   `xml:"biography"`
+	Died                string   `xml:"died"`
+	Disbanded           string   `xml:"disbanded"`
+	Thumb               []Thumb  `xml:"thumb"`
+	Path                string   `xml:"path"`
+}
+
+type Thumb struct {
+	Spoof   string `xml:"spoof,attr"`
+	Cache   string `xml:"cache,attr"`
+	Aspect  string `xml:"aspect,attr"`
+	Preview string `xml:"preview,attr"`
+	Value   string `xml:",chardata"`
 }
